3947bbca6ffe02beab133385b04e4c1fad50fae6
fxbrazier <fx.brazier@gmail.com>
2018-02-15 08:51:41 +0100

    modifs models

--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,10 @@
 var mongoose = require('mongoose')
 
 var userSchema = new mongoose.Schema({
-    name: {type: String, unique: true, sparse: true},
-    email: {type: String, unique: true, sparse: true},
+    name: {type: String, unique: true, sparse: true, required: true},
+    email: {type: String, unique: true, sparse: true, required: true},
     password: {type: String, required: true},
-    pokemonsCaptures: [
-    	{
-        	niveauEvolution: Number,
-        	evolutionId: mongoose.Pokemons.objectId,
-        	ref: 'Pokemon',
-    	}
-    ]
+    pokemonsCaptures: [Number]
 })
 
 var User = mongoose.model('User', userSchema)
