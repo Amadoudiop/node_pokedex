4edf4a624fa8e0fb643d2becd03a2a9d6a4e306f
fxbrazier <fx.brazier@gmail.com>
2018-02-15 10:17:38 +0100

    modif controllers

--- server/controllers/PokemonController.js
+++ server/controllers/PokemonController.js
@@ -1,3 +1,70 @@
 var mongoose = require("mongoose");
 
 var Pokemon = mongoose.model("Pokemon");
+
+var pokemonController = {};
+
+//show all pokemon
+pokemonController.list = function(req, res) {
+  Pokemon.find({}).exec(function (err, pokemons) {
+    if (err) {
+      console.log("Error:", err);
+    }
+    else {
+      res.render("../views/pokemons/index", {pokemons: pokemons});
+    }
+  });
+};
+
+//show one pokemon
+pokemonController.show = function(req, res) {
+  Pokemon.findOne({_id: req.params.id}).exec(function (err, pokemon) {
+    if (err) {
+      console.log("Error:", err);
+    }
+    else {
+      res.render("../views/pokemons/show", {pokemon: pokemon});
+    }
+  });
+};
+
+//create pokemon
+pokemonController.save = function(req, res) {
+  var pokemon = new Pokemon(req.body);
+
+  pokemon.save(function(err) {
+    if(err) {
+      console.log(err);
+      res.render("../views/pokemons/create");
+    } else {
+      console.log("Successfully created a pokemon.");
+      res.redirect("/pokemons/show/"+pokemon._id);
+    }
+  });
+};
+
+//update pokemon
+pokemonController.update = function(req, res) {
+  Pokemon.findByIdAndUpdate(req.params.id, { $set: { name: req.body.name, address: req.body.address, position: req.body.position, salary: req.body.salary }}, { new: true }, function (err, pokemon) {
+    if (err) {
+      console.log(err);
+      res.render("../views/pokemons/edit", {pokemon: req.body});
+    }
+    res.redirect("/pokemons/show/"+pokemon._id);
+  });
+};
+
+//delete pokemon
+pokemonController.delete = function(req, res) {
+  Pokemon.remove({_id: req.params.id}, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log("Pokemon deleted!");
+      res.redirect("/pokemons");
+    }
+  });
+};
+
+module.exports = pokemonController;
--- server/controllers/TypeController.js
+++ server/controllers/TypeController.js
@@ -1,3 +1,68 @@
 var mongoose = require("mongoose");
 
 var Type = mongoose.model("Type");
+
+//show all type
+typeController.list = function(req, res) {
+  Type.find({}).exec(function (err, types) {
+    if (err) {
+      console.log("Error:", err);
+    }
+    else {
+      res.render("../views/types/index", {types: types});
+    }
+  });
+};
+
+//show one type
+typeController.show = function(req, res) {
+  Type.findOne({_id: req.params.id}).exec(function (err, type) {
+    if (err) {
+      console.log("Error:", err);
+    }
+    else {
+      res.render("../views/types/show", {type: type});
+    }
+  });
+};
+
+//create type
+typeController.save = function(req, res) {
+  var type = new Type(req.body);
+
+  type.save(function(err) {
+    if(err) {
+      console.log(err);
+      res.render("../views/types/create");
+    } else {
+      console.log("Successfully created a type.");
+      res.redirect("/types/show/"+type._id);
+    }
+  });
+};
+
+//update type
+typeController.update = function(req, res) {
+  Type.findByIdAndUpdate(req.params.id, { $set: { name: req.body.name, address: req.body.address, position: req.body.position, salary: req.body.salary }}, { new: true }, function (err, type) {
+    if (err) {
+      console.log(err);
+      res.render("../views/types/edit", {type: req.body});
+    }
+    res.redirect("/types/show/"+type._id);
+  });
+};
+
+//delete type
+typeController.delete = function(req, res) {
+  Type.remove({_id: req.params.id}, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log("Type deleted!");
+      res.redirect("/types");
+    }
+  });
+};
+
+module.exports = typeController;
